eb5fcc6dd16032c513b014c940329fb6f23e1eca
Evandro Bernardes <s.evandro@hotmail.com>
2021-12-31 22:29:33 +0100

    Traditional masu box implemented with help of general maxu box script

--- OrigamiPatterns/Boxes_Masu.py
+++ OrigamiPatterns/Boxes_Masu.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import math
 import numpy as np
 from math import pi
 
@@ -23,7 +24,11 @@ def reflections_rect(path):
     new_paths = [path]
     new_paths = new_paths + Path.list_reflect(new_paths, (0, 0), (0, 1))
     return new_paths + Path.list_reflect(new_paths, (0, 0), (1, 0))
-    
+
+def recenter(paths, dist):
+    # paths_new = Path.list_simplify(paths)
+    paths_new = Path.list_rotate(paths, pi/4)
+    return Path.list_add(paths_new, (dist, dist))
 
 class MasuBox(Pattern):
 
@@ -33,8 +38,9 @@ class MasuBox(Pattern):
         Pattern.__init__(self)  # Must be called in order to parse common options
 
         # save all custom parameters defined on .inx file
-        self.add_argument('--pattern', type=self.str, default='template1')
-        self.add_argument('--length', type=self.float, default=10.0)
+        self.add_argument('--pattern', type=self.str, default='boxes_masu')
+        self.add_argument('--width', type=self.float, default=10.0)
+        self.add_argument('--height', type=self.float, default=10.0)
         self.add_argument('--sim', type=self.bool, default=True)
 
     def generate_path_tree(self):
@@ -44,42 +50,43 @@ class MasuBox(Pattern):
         unit_factor = self.calc_unit_factor()
 
         # retrieve saved parameters, and apply unit factor where needed
-        length = self.options.length * unit_factor
-        height = length
-        width = length
+        # length = self.options.length * unit_factor
+        width = self.options.width * unit_factor
+        height = self.options.height * unit_factor
+        length = math.sqrt(2)*(width + 2*height)
         half_fold = 90 if self.options.sim else 180
 
-        # generate all valleys
-        corner_valleys = reflections_diag(Path([(0, -1), (0, -2), (-0.5, -1.5)], 'v'))
-        short_valleys = [
-            Path([(+2, +1), (+1, +2)], 'v', fold_angle = half_fold),
-            Path([(-2, -1), (-1, -2)], 'v', fold_angle = half_fold)]
-        long_valleys = [
-            Path([(+0.5, -1.5), (+1.0, -2.0), (+2.0, -1.0), (+1.5, -0.5)], 'v', fold_angle = half_fold),
-            Path([(-0.5, +1.5), (-1.0, +2.0), (-2.0, +1.0), (-1.5, +0.5)], 'v', fold_angle = half_fold)]
-        valleys = Path.list_simplify([corner_valleys, long_valleys, short_valleys])
+        # bottom of box
+        ring_inner = Path.generate_square(width, width, center = [0,0], style = 'm', fold_angle=half_fold)
 
-        # generate all mountains
-        center_square = Path([(-1, 0.00), (0.00, -1), (+1, 0.00), (0.00, +1)], 'm', closed=True, fold_angle=half_fold)
-        short_mountains = reflections_diag(Path([(0, -1), (0.5, -1.5)], 'm', fold_angle=half_fold))
-        vertical_long_mountains = [
-            Path([(-2, 0), (0, +2)], 'm'),
-            Path([(0, -2), (+2, 0)], 'm')]
-        vertical_medium_mountains = reflections_diag(Path([(-1, -2), (0, -1)], 'm'))
-        horizontal_mountains = [
-            Path([(-1.5, -0.5), (-0.5, -1.5)], 'm'),
-            Path([(+1.5, +0.5), (+0.5, +1.5)], 'm')]
-        mountains = Path.list_simplify([center_square, short_mountains, horizontal_mountains,
-                                        vertical_medium_mountains, vertical_long_mountains])
+        # ring making the corners
+        lengths = [height, width, height]
+        points = Path.get_square_points(sum(lengths), sum(lengths), [0,0])
+        styles = ['vmv','mmm']
+        ring_middle = [Path([points[i], points[(i + 1)%4]], 'm').
+                           break_path(lengths, styles[i % 2]) for i in range(4)]
 
-        # decenter and scale
-        valleys = Path.list_mul(valleys, [length / 4])
-        valleys = Path.list_add(valleys, (length / 2, length / 2))
-        mountains = Path.list_mul(mountains, [length / 4])
-        mountains = Path.list_add(mountains, (length / 2, length / 2))
+        # arms along width and length
+        points = [(-width / 2, -(width / 2 + 0 * height)),
+                  (-width / 2, -(width / 2 + 1 * height)),
+                  (-width / 2, -(width / 2 + 2 * height)),
+                  (+width / 2, -(width / 2 + 2 * height)),
+                  (+width / 2, -(width / 2 + 1 * height)),
+                  (+width / 2, -(width / 2 + 0 * height))]
 
-        self.edge_points = Path.generate_square(length, length).points
-        self.path_tree = [valleys, mountains]
+        arms_ = [Path.list_create_from_points(points, 'mmvmm', fold_angles = [180, 180, half_fold, 180, 180]),
+                 Path.list_create_from_points(points, 'mvvvm', half_fold)]
+        arms = [Path.list_rotate(arms_[i % 2], i * pi / 2) for i in range(4)]
+
+        # tiny corner diagonals
+        diag = Path([(width/2, width/2), (width/2 + height, width/2 + height)], 'v')
+        corner_diagonals = [diag * (1, i*pi/2) for i in range(4)]
+
+        self.edge_points = Path.get_square_points(length, length)
+
+        self.path_tree = recenter(
+            [ring_inner, ring_middle, arms, corner_diagonals],
+            length/2)
 
 # Main function, creates an instance of the Class and calls self.draw() to draw the origami on inkscape
 # self.draw() is either a call to inkex.affect() or to svg.run(), depending on python version
